d56b801a1941dbdf097de505235b9eac657b9c83
Tigran <tigranshahbazyan2019@gmail.com>
2025-07-10 22:24:26 +0400

    Новое сообщение коммита

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-function movingFirstMenu() {
-    const firstMenu = document.getElementById("first");
-    const currentTransform = getComputedStyle(firstMenu).transform;
-
-    if (currentTransform === 'matrix(1, 0, 0, 1, 0, -100)') {
-        firstMenu.style.animation = "dropBounce 0.8s ease forwards";
-    } else {
-        firstMenu.style.animation = "jumpUp 0.8s ease forwards";
-    }
-}
 
 
