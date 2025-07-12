793fe111fd688933e020326bf64939452a6a4844
Tigran <tigranshahbazyan2019@gmail.com>
2025-07-12 22:48:05 +0400

    updated header and created new temporary menu

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+function openRightMenu() {
+    document.getElementById("mediaMenuOnClick").style.top = "100px";
+    document.getElementById("jamanakavorDelete").style.display = "none";
+
+}
+function closeRightMenu() {
+    document.getElementById("mediaMenuOnClick").style.top = "-400px";
+    document.getElementById("jamanakavorDelete").style.display = "block";
 
 
+}
