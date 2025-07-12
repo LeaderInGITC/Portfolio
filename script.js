91a1e708bd9d4d81542a31b277895d1d8fc6f0df
Ararat01 <ararat12bars@gmail.com>
2025-07-13 01:10:09 +0400

    new menu animation

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function openRightMenu() {
-    document.getElementById("mediaMenuOnClick").style.top = "100px";
-    document.getElementById("jamanakavorDelete").style.display = "none";
-
+  document.getElementById("mediaMenuOnClick").style.top = "100px";
+  document.getElementById("mediaMenuOnClick").style.opacity = "1";
 }
 function closeRightMenu() {
-    document.getElementById("mediaMenuOnClick").style.top = "-400px";
-    document.getElementById("jamanakavorDelete").style.display = "block";
-
-
+  document.getElementById("mediaMenuOnClick").style.top = "-400px";
+  document.getElementById("mediaMenuOnClick").style.opacity = "0";
 }
