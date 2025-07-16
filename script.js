5f61043f7e25680b7b33b9c8f024053e43ec7f99
Tigran <tigranshahbazyan2019@gmail.com>
2025-07-16 21:15:41 +0400

    updated enough details Ararat jan

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function openRightMenu() {
   document.getElementById("mediaMenuOnClick").style.top = "100px";
   document.getElementById("mediaMenuOnClick").style.opacity = "1";
+  document.getElementById("first").style.opacity = "0";
+  
 }
 function closeRightMenu() {
   document.getElementById("mediaMenuOnClick").style.top = "-400px";
   document.getElementById("mediaMenuOnClick").style.opacity = "0";
+  document.getElementById("first").style.opacity = "1";
+
 }
